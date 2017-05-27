d5b9333b9445f9b87c55d66bbdf1f16056dde5a7
staffandev <staffan.ericson@zocom.se>
2017-05-27 05:28:59 +0200

    new site

---
+++ public/js/init.js
@@ -0,0 +1,19 @@
+(function ($) {
+  $(function () {
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    var nav = $('#main-menu-container');
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 400) {
+        nav.addClass("f-nav");
+      } else {
+        nav.removeClass("f-nav");
+      }
+
+    });
+  
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
